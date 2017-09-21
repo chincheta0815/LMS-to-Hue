4738592b4255c6b40826fba4fe5f05aa164e79db
chincheta0815 <rouven_weiler@gmx.net>
2017-09-21 20:49:45 +0200

    Fix minor typos and layout

--- helper/LMS-to-Hue/virtual/virtual.h
+++ helper/LMS-to-Hue/virtual/virtual.h
@@ -1,12 +1,12 @@
-/*****************************************************************************
- * rtsp_client.h: RAOP Client
+/*
+ * virtual.h: Virtual player
  *
  * Copyright (C) 2004 Shiro Ninomiya <shiron@snino.com>
  *				 2016 Philippe <philippe_44@outlook.com>
  *
- * This program is free software; you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -15,9 +15,9 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111, USA.
- *****************************************************************************/
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
 
 #ifndef __VIRTUAL_H_
